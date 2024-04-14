a8713705484e722e26e38f120cbec2fac1dd4766
Joao Paulo Mazzo <joao.mazzo@nexum.com.br>
2024-04-13 23:29:34 -0300

    creating button component

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule],
+  templateUrl: './button.component.html',
+})
+export class ButtonComponent {
+
+}
