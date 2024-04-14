79fd10730c8f11922251aeea557c6b7ba46a576c
Joao Paulo Mazzo <joao.mazzo@nexum.com.br>
2024-04-13 23:29:22 -0300

    creating invoice-list component

---
+++ src/app/feature/invoice-list/invoice-list.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { InvoiceFormComponent } from '../../shared/components/invoice-form/invoice-form.component';
+
+@Component({
+  selector: 'app-invoice-list',
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    ButtonComponent,
+    MatCheckboxModule,
+    InvoiceFormComponent
+  ],
+  templateUrl: './invoice-list.component.html',
+})
+export class InvoiceListComponent {
+  sidebarVisible: boolean = false;
+
+  filterByStatus = this._formBuilder.group({
+    draft: false,
+    pending: false,
+    paid: false,
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
